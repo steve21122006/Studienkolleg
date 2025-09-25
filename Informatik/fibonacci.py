9d7baa81f8649665aacabd9d15d2d64d149af6aa
tl06 <96576022+steve21122006@users.noreply.github.com>
2025-09-25 22:25:18 +0200

    Update fibonacci.py

--- Informatik/fibonacci.py
+++ Informatik/fibonacci.py
@@ -1,8 +1,9 @@
+c = 100# 100 ist für die Berechnung der ersten (100+2) Fibonacci-Zahlen
 a = 0
 b = 1
 print(a)
 print(b)
-for _ in range(1, 100): # 100 ist für die Berechnung der ersten (100+2) Fibonacci-Zahlen
+for _ in range(1, c): # 100 ist für die Berechnung der ersten (100+2) Fibonacci-Zahlen
     a = a + b
     print(a)
     b = b + a
