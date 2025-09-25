2adda96a8926e1477017d2d972a2f1eb4beaea5d
tl06 <96576022+steve21122006@users.noreply.github.com>
2025-09-25 15:57:07 +0700

    Update summeeinergeradenreihe.py

--- Informatik/summeeinergeradenreihe.py
+++ Informatik/summeeinergeradenreihe.py
@@ -1,11 +1,11 @@
 b = 11
 summe = 0
 
-if b % 2 == 0:  # odd
-    for a in range(2, b, 2):
+if b % 2 == 0:  # even
+    for a in range(2, b + 1, 2):
         summe += a
-else:       # even
-    for a in range(2, b-1, 2):
+else:       # odd
+    for a in range(2, b, 2):
         summe += a
 
 print(summe)
